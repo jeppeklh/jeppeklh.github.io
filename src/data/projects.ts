75c1b32a60c7c9c8e2c82949a55030f4c9452d2b
Jeppe Lynge Hansen <jeppeklh@hotmail.com>
2025-09-07 14:28:17 +0200

    fix: changes typo

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -13,7 +13,7 @@ export const projects: Project[] = [
   {
     title: "Parkeringsforbudsregistrering (Odense Kommune)",
     description:
-      "Som en del af specialiseringsprojektet er jeg sammen med mit team i gang med atudvikle et system til registrering af biler i områder med midlertidige parkeringsforbud i samarbejde med Odense Kommune.\nLøsningen skal gøre det muligt at scanne biler og automatisk hente nummerplade, bilmærke/model, farve og geolokation. Data samles i en oversigt, genererer en PDF og sendes direkte til politiet fra felten.\nJeg har ansvar for app-udviklingen (frontend og backend, inkl. integrationer) samt en Python-baseret machine learning-service til billedgenkendelse.",
+      "Som en del af specialiseringsprojektet er jeg sammen med mit team i gang med at udvikle et system til registrering af biler i områder med midlertidige parkeringsforbud i samarbejde med Odense Kommune.\nLøsningen skal gøre det muligt at scanne biler og automatisk hente nummerplade, bilmærke/model, farve og geolokation. Data samles i en oversigt, genererer en PDF og sendes direkte til politiet fra felten.\nJeg har ansvar for app-udviklingen (frontend og backend, inkl. integrationer) samt en Python-baseret machine learning-service til billedgenkendelse.",
 
     tech: [
       "React",
